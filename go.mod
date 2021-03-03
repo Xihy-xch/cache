425b2501d7f78213d197cb7ab1c6db256c2a579f
xch <xingchanghao@baijia.com>
2021-03-03 16:43:39 +0800

    实现cache接口

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module local-cache
 
 go 1.15
 
-require golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+require (
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+)
--- local_cache.go
+++ local_cache.go
@@ -3,6 +3,7 @@ package local_cache
 import (
 	"errors"
 	"fmt"
+	lru "github.com/hashicorp/golang-lru"
 	"golang.org/x/sync/singleflight"
 	"sync"
 	"time"
@@ -19,7 +20,14 @@ func (i *item) getDefaultOptions() *Options {
 	}
 }
 
-type Cache struct {
+type Cache interface {
+	Get(key string) (interface{}, error)
+	Set(key string, val interface{}, opts ...OptionsFn)
+	Delete(key string)
+	Clean()
+}
+
+type DefaultCache struct {
 	valueMap map[string]item
 	options  *Options
 	rwMutex  *sync.RWMutex
@@ -27,8 +35,8 @@ type Cache struct {
 	ticker   *time.Ticker
 }
 
-func NewCache(opts ...OptionsFn) *Cache {
-	c := &Cache{
+func NewCache(opts ...OptionsFn) *DefaultCache {
+	c := &DefaultCache{
 		valueMap: make(map[string]item),
 		rwMutex:  new(sync.RWMutex),
 		sf:       new(singleflight.Group),
@@ -40,37 +48,37 @@ func NewCache(opts ...OptionsFn) *Cache {
 		opt(o)
 	}
 	c.options = o
-	go c.clean()
+	go c.Clean()
 	return c
 }
 
-func (c *Cache) getDefaultOptions() *Options {
+func (d *DefaultCache) getDefaultOptions() *Options {
 	return &Options{
 		expiration: 10 * time.Second,
 		maxSum:     1024,
 	}
 }
 
-func (c *Cache) Set(key string, val interface{}, opts ...OptionsFn) {
-	o := c.getDefaultOptions()
+func (d *DefaultCache) Set(key string, val interface{}, opts ...OptionsFn) {
+	o := d.getDefaultOptions()
 
 	for _, opt := range opts {
 		opt(o)
 	}
 
-	c.rwMutex.Lock()
-	defer c.rwMutex.Unlock()
-	c.valueMap[key] = item{
+	d.rwMutex.Lock()
+	defer d.rwMutex.Unlock()
+	d.valueMap[key] = item{
 		value:      val,
 		expiration: time.Now().Add(o.GetExpiration()),
 	}
 }
 
-func (c *Cache) Get(key string) (interface{}, error) {
-	val, err, _ := c.sf.Do(key, func() (interface{}, error) {
-		c.rwMutex.RLock()
-		defer c.rwMutex.RUnlock()
-		if val, exist := c.valueMap[key]; exist {
+func (d *DefaultCache) Get(key string) (interface{}, error) {
+	val, err, _ := d.sf.Do(key, func() (interface{}, error) {
+		d.rwMutex.RLock()
+		defer d.rwMutex.RUnlock()
+		if val, exist := d.valueMap[key]; exist {
 			return val, nil
 		}
 
@@ -83,35 +91,49 @@ func (c *Cache) Get(key string) (interface{}, error) {
 	return nil, err
 }
 
-// @todo error处理
-func (c *Cache) clean() error {
-	for {
-		<-c.ticker.C
-		fmt.Println("开始清理")
-		switch c.options.cleanMode {
-		case Default:
+func (d *DefaultCache) Delete(key string) {
+	d.rwMutex.Lock()
+	defer d.rwMutex.Unlock()
+	delete(d.valueMap, key)
+}
 
-			c.defaultClean()
+// @todo error处理
+func (d *DefaultCache) Clean() {
+	var err error
+	for {
+		<-d.ticker.C
+		fmt.Println("开始清理")
+		switch d.options.cleanMode {
+		case Default:
+			err = d.defaultClean()
 		case LRU:
-			c.lruClean()
+			err = d.lruClean()
 		default:
-			c.defaultClean()
+			err = d.defaultClean()
 		}
+		fmt.Println(err)
 	}
 }
 
-func (c *Cache) defaultClean() error {
+func (d *DefaultCache) defaultClean() error {
 	now := time.Now()
 
-	for key, item := range c.valueMap {
+	for key, item := range d.valueMap {
 		if now.After(item.expiration) {
-			delete(c.valueMap, key)
+			delete(d.valueMap, key)
 		}
 	}
 
 	return nil
 }
 
-func (c *Cache) lruClean() error {
+func (d *DefaultCache) lruClean() error {
+	l, _ := lru.New(128)
+	for i := 0; i < 256; i++ {
+		l.Add(i, nil)
+	}
+	if l.Len() != 128 {
+		panic(fmt.Sprintf("bad len: %v", l.Len()))
+	}
 	return nil
 }
